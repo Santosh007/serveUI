4e9a63d161cdacaaee80332fb847d0b4ddbb8bcd
santosh007 <santoshreddy119@gmail.com>
2018-01-28 20:22:29 +0530

    Updated the app with environment specific port details
    Signed-off-by: santosh007 <santoshreddy119@gmail.com>

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ var express = require("express");
 var path = require('path');
 var app = express();
 
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || process.env.PORT || 9000);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.OPENSHIFT_INTERNAL_IP || 'localhost');
+
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(9000);
-console.log("Express app running on port 9000");
+app.listen(app.get('port'), app.get('ip'), function(){
+  console.log("Express server listening on " + app.get('ip') + ":" + app.get('port'));
+});
+
 module.exports = app;
